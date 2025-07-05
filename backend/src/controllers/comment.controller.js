1f078c57b51294a669167558a7bc032b89d031d4
Marco Catarino <marco.catarino60@gmail.com>
2025-07-05 10:06:31 -0500

    style: Removing an import not used

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -1,8 +1,6 @@
 import asyncHandler from "express-async-handler";
 import { getAuth } from "@clerk/express";
 
-import cloudinary from "../config/cloudinary.js";
-
 import User from "../models/user.model.js";
 import Post from "../models/post.model.js";
 import Notification from "../models/notification.model.js";
