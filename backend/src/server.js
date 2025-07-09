161ae6c4b774791d441b4debfbb3096ce966127d
Marco Catarino <marco.catarino60@gmail.com>
2025-07-09 09:27:18 -0500

    log

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(clerkMiddleware());
 app.use(arcjetMiddleware); // We don't initialize '()' because it's going to be called by express when we run the Server by default
 
-app.get("/", (req, res) => res.send("Hello from Server 2.0")); // Prueba
+app.get("/", (req, res) => res.send("Hello from Server 2.1")); // Prueba
 
 // TODO: Routes
 app.use("/api/users", userRoutes);
